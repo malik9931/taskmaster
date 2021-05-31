b4064f4a576df882cdafdf2ff30d2fdc422f7227
Malek <engmalek.civil@gmail.com>
2021-05-31 16:46:19 +0300

    lab-32

--- README.md
+++ README.md
@@ -79,3 +79,14 @@ Ensure that the description and status of a tapped task are also displayed on th
 
 #### Task Detail
 ![Task Detail screenshots](screenshots/detail29.png)
+
+
+----------------------------------------------------------------------------
+
+### Lab: 32 - Amplify and DynamoDB
+
+#### Add Task Form
+Modify your Add Task form to save the data entered in as a Task to DynamoDB.
+
+#### Homepage
+Refactor your homepage’s RecyclerView to display all Task entities in DynamoDB.
--- app/src/main/java/com/example/taskmasterapp/AddTaskActivity.java
+++ app/src/main/java/com/example/taskmasterapp/AddTaskActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.RadioButton;
@@ -11,7 +12,11 @@ import android.widget.RadioGroup;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.example.taskmasterapp.Models.Task;
+import com.amplifyframework.AmplifyException;
+import com.amplifyframework.core.Amplify;
+import com.amplifyframework.datastore.AWSDataStorePlugin;
+import com.amplifyframework.datastore.generated.model.State;
+import com.amplifyframework.datastore.generated.model.Task;
 
 public class AddTaskActivity extends AppCompatActivity {
 
@@ -21,6 +26,15 @@ public class AddTaskActivity extends AppCompatActivity {
         setContentView(R.layout.activity_add_task);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setTitle("Task Master");
+
+//        try {
+//            Amplify.addPlugin(new AWSDataStorePlugin());
+//            Amplify.configure(getApplicationContext());
+//
+//            Log.i("Tutorial", "Initialized Amplify in ADD TASK Activity");
+//        } catch (AmplifyException e) {
+//            Log.e("Tutorial", "Could not initialize Amplify in ADD TASK Activity", e);
+//        }
 
         // Get the Intent that started this activity
         Intent intent = getIntent();
@@ -37,17 +51,19 @@ public class AddTaskActivity extends AppCompatActivity {
                 int radioId = radioGroup.getCheckedRadioButtonId();
                 RadioButton radioButton = findViewById(radioId);
 
-
-                if (titleTextView.getText() == "" ||bodyTextView.getText() == null ){
-                    Toast.makeText(AddTaskActivity.this,"some thing",Toast.LENGTH_SHORT).show();
-                }else {
                     totalTaskTextView.setText("Total Sorted Task: "+AppDataBase.getInstance(getApplicationContext()).taskDao().getAllTasks().size());
 
-                    Task task = new Task(titleTextView.getText().toString(),bodyTextView.getText().toString(),radioButton.getText().toString());
-                    AppDataBase.getInstance(getApplicationContext()).taskDao().addTask(task);
-                    Toast.makeText(AddTaskActivity.this,"U added a new Task",Toast.LENGTH_SHORT).show();
-                }
+//                    Task task = new Task(titleTextView.getText().toString(),bodyTextView.getText().toString(),radioButton.getText().toString());
+//                    AppDataBase.getInstance(getApplicationContext()).taskDao().addTask(task);
+//                    Toast.makeText(AddTaskActivity.this,"U added a new Task",Toast.LENGTH_SHORT).show();
 
+                Task task= Task.builder().title(titleTextView.getText().toString()).body(bodyTextView.getText().toString()).state(State.NEW).build();
+
+                Amplify.DataStore.save(task,
+                        success -> Log.i("Tutorial", "Saved item: " + success.item().getTitle()),
+                        error -> Log.e("Tutorial", "Could not save item to DataStore", error)
+                );
+                finish();
             }
         });
     }
